0059b0dbad0f1e31519d1e1a8fa87ad59e7f2600
Joe Crowley <crowley@ucsb.edu>
2022-12-13 14:42:03 -0800

    add stack macro

--- cpp/stack_mc_categorized.C
+++ cpp/stack_mc_categorized.C
@@ -2,15 +2,14 @@
 gROOT->ProcessLine(".L analyze_bjets.C+");
 
 
-std::string hname("njet");
 std::string plotDir("/home/users/crowley/public_html/test");
 vector<string> rootFiles;
-rootFiles.push_back("outputs/mc/output_TT_2l2nu.root");
-rootFiles.push_back("outputs/mc/output_TT_lnu.root");
-rootFiles.push_back("outputs/mc/output_TTV.root");
-rootFiles.push_back("outputs/mc/output_ST_tW.root");
-rootFiles.push_back("outputs/mc/output_WW.root");
 rootFiles.push_back("outputs/mc/output_Others.root");
+rootFiles.push_back("outputs/mc/output_WW.root");
+rootFiles.push_back("outputs/mc/output_ST_tW.root");
+rootFiles.push_back("outputs/mc/output_TTV.root");
+rootFiles.push_back("outputs/mc/output_TT_lnu.root");
+rootFiles.push_back("outputs/mc/output_TT_2l2nu.root");
 
 stackHists("njet", rootFiles, plotDir.data());
 stackHists("Ht", rootFiles, plotDir.data());
