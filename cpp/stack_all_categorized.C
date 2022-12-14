7787965732bab995a8d79a92b93d3b18c6e3d8c8
Joe Crowley <crowley@ucsb.edu>
2022-12-13 16:14:02 -0800

    add data

--- cpp/stack_all_categorized.C
+++ cpp/stack_all_categorized.C
@@ -4,17 +4,22 @@ gROOT->ProcessLine(".L analyze_bjets.C+");
 
 std::string plotDir("/home/users/crowley/public_html/test");
 vector<string> rootFiles;
-rootFiles.push_back("outputs/mc/output_Others.root");
-rootFiles.push_back("outputs/mc/output_WW.root");
-rootFiles.push_back("outputs/mc/output_ST_tW.root");
-rootFiles.push_back("outputs/mc/output_TTV.root");
-rootFiles.push_back("outputs/mc/output_TT_lnu.root");
-rootFiles.push_back("outputs/mc/output_TT_2l2nu.root");
+rootFiles.push_back("outputs/mc/hists_Others.root");
+rootFiles.push_back("outputs/mc/hists_WW.root");
+rootFiles.push_back("outputs/mc/hists_ST_tW.root");
+rootFiles.push_back("outputs/mc/hists_TTV.root");
+rootFiles.push_back("outputs/mc/hists_TT_lnu.root");
+rootFiles.push_back("outputs/mc/hists_TT_2l2nu.root");
 rootFiles.push_back("outputs/data/hists_Data.root");
 
 stackHists("njet", rootFiles, plotDir.data());
-stackHists("Ht", rootFiles, plotDir.data());
 stackHists("met", rootFiles, plotDir.data());
-
+stackHists("Ht", rootFiles, plotDir.data());
+stackHists("lep1_pt", rootFiles, plotDir.data());
+stackHists("lep1_eta", rootFiles, plotDir.data());
+stackHists("lep1_phi", rootFiles, plotDir.data());
+stackHists("lep2_pt", rootFiles, plotDir.data());
+stackHists("lep2_eta", rootFiles, plotDir.data());
+stackHists("lep2_phi", rootFiles, plotDir.data());
 
 }
