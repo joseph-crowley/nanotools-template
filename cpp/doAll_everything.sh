37e226d42f37576faacfd1a65b22b5c9946d359b
Joe Crowley <crowley@ucsb.edu>
2023-02-21 16:41:46 -0800

    backup existing results

--- cpp/doAll_everything.sh
+++ cpp/doAll_everything.sh
@@ -1,12 +1,26 @@
 cd ~/tttt/CMSSW_10_6_26/src/tttt; cmsenv; eval $(./setup.sh env); cd -
 
-# Data
+# back up the logs/ and output/ directories if they exist
+if [ -d logs/ ] && [ -d outputs/ ]; then
+    # backup the existing directories
+    backup_num=$(ls -d logs_backup_* | wc -l)
+    mkdir "logs_backup_$backup_num"
+    mv logs/ "logs_backup_$backup_num/"
+    mkdir "outputs_backup_$backup_num"
+    mv outputs/ "outputs_backup_$backup_num/"
+fi
+
+# create the logs/ and outputs/ directories if they don't exist
+mkdir -p logs/
+mkdir -p outputs/ outputs/data outputs/mc outputs/plots
+
+# Make Histograms: Data
 rqlb doAll_data_NonAPV_2016.C > logs/doAll_data_NonAPV_2016.log 2>&1 &
 rqlb doAll_data_APV_2016.C > logs/doAll_data_APV_2016.log 2>&1 &
 rqlb doAll_data_2017.C > logs/doAll_data_2017.log 2>&1 &
 rqlb doAll_data_2018.C > logs/doAll_data_2018.log 2>&1 &
 
-# MC 
+# Make Histograms: MC
 rqlb doAll_mc_2016_NonAPV.C > logs/doAll_mc_2016_NonAPV.log 2>&1 &
 rqlb doAll_mc_2016_APV.C > logs/doAll_mc_2016_APV.log 2>&1 &
 rqlb doAll_mc_2017.C > logs/doAll_mc_2017.log 2>&1 &
@@ -17,7 +31,7 @@ echo "All histogramming scripts have begun. View the logs in logs/*"
 wait 
 echo "All histogramming scripts have completed. Beginning to plot..."
 
-# plotting
+# Plotting the Histograms 
 rqlb stack_2016_NonAPV.C > logs/stack_2016_NonAPV.log 2>&1 &
 rqlb stack_2016_APV.C > logs/stack_2016_APV.log 2>&1 &
 rqlb stack_2017.C > logs/stack_2017.log 2>&1 &
